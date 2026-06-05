e83d4c18e611a84ca584598ddec8e28fc8ae7df0
alanramadhani2112 <alanramadhani2112@users.noreply.github.com>
2026-06-05 16:51:50 +0700

    fix(ui): switch icon component from ki-duotone to ki-solid

--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -18,10 +18,4 @@
     $iconName = $aliases[$name] ?? $name;
 @endphp
 
-<i {{ $attributes->merge(['class' => "ki-duotone ki-{$iconName}"]) }}>
-    <span class="path1"></span>
-    <span class="path2"></span>
-    <span class="path3"></span>
-    <span class="path4"></span>
-    <span class="path5"></span>
-</i>
+<i {{ $attributes->merge(['class' => "ki-solid ki-{$iconName}"]) }}></i>
